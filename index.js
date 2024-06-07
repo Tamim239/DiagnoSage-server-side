3a0949cd2516953dc7f1ab77da3cfefdfb267849
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-06-07 16:07:34 +0600

    added testCollection

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 // jwt verify middleware
-const verifyToken = (req, res, next) =>{
+const verifyToken = (req, res, next) => {
   console.log('inside verify token', req.headers.authorization)
-  if(!req.headers.authorization){
-return res.status(401).send({message: 'unauthorized access'});
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'unauthorized access' });
   }
   const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_API, (err, decoded)=>{
-    if(err){
-      return res.status(401).send({message: 'unauthorized access'});
+  jwt.verify(token, process.env.ACCESS_TOKEN_API, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' });
     }
     req.decoded = decoded;
     next()
   })
 }
 // verify admin after verifyToken
-const verifyAdmin = async (req, res, next) =>{
+const verifyAdmin = async (req, res, next) => {
   const email = req.decoded.email;
-  const query = {email};
+  const query = { email };
   const user = await userCollection.findOne(query);
   const isAdmin = user?.role === 'admin'
-  if(!isAdmin){
-    return res.status(403).send({message: 'forbidden access'});
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'forbidden access' });
   }
   next()
- }
+}
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.uj1q2ho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -56,28 +56,29 @@ async function run() {
 
     const userCollection = client.db('DaignoDb').collection('users');
     const bannerCollection = client.db('DaignoDb').collection('banners');
+    const testCollection = client.db('DaignoDb').collection('tests');
     // auth related api
-    app.post('/jwt', async (req, res) =>{
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_API,{ expiresIn: '1h'})
-      res.send({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_API, { expiresIn: '1h' })
+      res.send({ token })
     });
     // Logout
     app.get("/logout", async (req, res) => {
       const user = req.body;
       console.log("logging out", user);
       res
-        .clearCookie("token", { 
+        .clearCookie("token", {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-           maxAge: 0 
-      })
+          maxAge: 0
+        })
         .send({ success: true });
     });
 
     // user collection
-    app.get('/users',verifyToken, verifyAdmin, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result)
     });
@@ -88,41 +89,59 @@ async function run() {
       res.send(result)
     });
 
-// admin set
-app.get('/user/admin/:email', verifyToken, async(req, res) =>{
-  const email = req.params.email;
-  if(email !== req.decoded.email){
-    return res.status(403).send({message: 'unauthorized access'})
-  }
-  const query = {email: email}
-  const user = await userCollection.findOne(query);
-  let admin = false;
-  if(user){
-    admin = user?.role === 'admin';
-  }
-  res.send({admin})
+    // admin set
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin })
+    });
+
+    // added banner info and set get delete
+    app.get('/banners', async (req, res) => {
+      const result = await bannerCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.post('/banners', async (req, res) => {
+      const banner = req.body;
+      const result = await bannerCollection.insertOne(banner);
+      res.send(result)
+    });
+
+    app.delete('/banners/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await bannerCollection.deleteOne(query);
+      res.send(result)
+    });
+
+    // add test set get delete update
+app.get('/tests', async(req, res)=>{
+  const result = await testCollection.find().toArray();
+  res.send(result);
 });
 
-// added banner info and set get delete
-app.get('/banners', async(req, res)=>{
-  const result = await bannerCollection.find().toArray();
-  res.send(result)
-});
-
-app.post('/banners', async(req, res)=>{
-  const banner = req.body;
-  const result = await bannerCollection.insertOne(banner);
-  res.send(result)
-});
-
-app.delete('/banners/:id', async(req, res)=>{
+app.get('/tests/:id', async(req, res) =>{
   const id = req.params.id;
-  console.log(id)
   const query = {_id: new ObjectId(id)};
-  const result = await bannerCollection.deleteOne(query);
+  const result = await testCollection.findOne(query);
   res.send(result)
-});
+})
 
+    app.post('/tests', async (req, res) => {
+      const test = req.body;
+      const result = await testCollection.insertOne(test);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
